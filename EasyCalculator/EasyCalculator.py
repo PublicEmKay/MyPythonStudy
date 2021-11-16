7c97f0585a52976f6c930e9184bc3b7874a2ee66
PublicEmKay <94438040+PublicEmKay@users.noreply.github.com>
2021-11-16 17:39:02 +0200

    Оформлення кольорової теми

--- EasyCalculator/EasyCalculator.py
+++ EasyCalculator/EasyCalculator.py
@@ -3,10 +3,17 @@
 from colorama import init
 from colorama import Fore, Back, Style
 
+init()
+
+print(Back.GREEN)
+print(Fore.BLACK)
 what = input("Що робимо? (+ - * /):\n")
 
 a = float(input("Введіть перше число:\n"))
 b = float(input("Введіть друге число:\n"))
+
+print(Back.LIGHTBLUE_EX)
+print(Fore.BLACK)
 
 if what == "+":
     c = a + b
@@ -21,4 +28,6 @@ elif what == "/":
     c = a / b
     print("Результат: " + str(c))
 else:
+    print(Back.RED)
+    print(Fore.BLACK)
     print("Нажаль даний функціонал недоступний(")
